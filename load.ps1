d94dba4727c504a5f301d046949f29cb2c1adea4
nixie <155293735+nixielock@users.noreply.github.com>
2025-05-13 13:19:33 +0800

    update load.ps1 for avd, update helix config + add wip theme

--- load.ps1
+++ load.ps1
@@ -137,7 +137,7 @@ if (!$profileNoClear) {
 }
 
 # display greeting
-pwsh-greeting $profileTimer.Elapsed.TotalSeconds -f -c
+pwsh-greeting $profileTimer.Elapsed.TotalSeconds -f:$((hostname) -inotmatch '^avd.*') -c
 
 # cleanup profile variables
 rv profileTimer, iterationCount, profileNoClear
